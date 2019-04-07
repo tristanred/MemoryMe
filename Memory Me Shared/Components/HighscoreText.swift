a221ce921de122ca42ce20ecf3f50b54db2a74c6
Tristan Dubé <tristan.dube@gmail.com>
2019-04-07 15:29:39 -0400

    Cleaned Highscore class
    Moved the properties to be non-optional so I had to duplicate the code in the NSCoder class init.

--- Memory Me Shared/Components/HighscoreText.swift
+++ Memory Me Shared/Components/HighscoreText.swift
@@ -11,36 +11,49 @@ import SpriteKit
 
 class HighscoreText : SKNode
 {
-    public var textLabel: SKLabelNode?;
+    public var textLabel: SKLabelNode;
     
-    private var fadeInAction: SKAction?;
+    private var fadeInAction: SKAction;
     
     override init() {
-        super.init();
-        
         textLabel = SKLabelNode(text: "");
-        textLabel?.fontSize = 96;
-        textLabel?.alpha = 0;
-        self.addChild(textLabel!);
+        textLabel.fontSize = 96;
+        textLabel.alpha = 0;
         
         let fadeIn = SKAction.fadeIn(withDuration: 1);
         let delay = SKAction.wait(forDuration: 1);
         let fadeOut = SKAction.fadeOut(withDuration: 1);
         
         self.fadeInAction = SKAction.sequence([fadeIn, delay, fadeOut]);
+
+        super.init();
+        
+        self.addChild(textLabel);
     }
     
     required init?(coder aDecoder: NSCoder) {
+        textLabel = SKLabelNode(text: "");
+        textLabel.fontSize = 96;
+        textLabel.alpha = 0;
+        
+        let fadeIn = SKAction.fadeIn(withDuration: 1);
+        let delay = SKAction.wait(forDuration: 1);
+        let fadeOut = SKAction.fadeOut(withDuration: 1);
+        
+        self.fadeInAction = SKAction.sequence([fadeIn, delay, fadeOut]);
+
         super.init(coder: aDecoder);
+
+        self.addChild(textLabel);
     }
     
     public func setText(_ scoreAmount: Int)
     {
-        self.textLabel?.text = "Today's highscore = \(scoreAmount)";
+        self.textLabel.text = "Today's highscore = \(scoreAmount)";
     }
     
     public func temporaryShow()
     {
-        textLabel?.run(fadeInAction!);
+        textLabel.run(fadeInAction);
     }
 }
